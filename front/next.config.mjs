b77a174d53836c611096c65ed986a831bf3624e2
KaGH6 <kam85933@gmail.com>
2025-06-19 21:00:00 +0900

    updated

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
     images: {
         // domains: ['http://127.0.0.1:8000'],
         domains: [
-            'ekadon-bucket.s3.ap-northeast-1.amazonaws.com'
+            'ekadon-bucket.s3.ap-northeast-1.amazonaws.com',
+            's3.ap-northeast-1.amazonaws.com'
         ]
     },
 };
