37d881567b304f9dfb805122eccb2105433e90e8
KaGH6 <kam85933@gmail.com>
2025-06-20 20:33:52 +0900

    Update CategoryController.php

--- back/app/Http/Controllers/CategoryController.php
+++ back/app/Http/Controllers/CategoryController.php
@@ -31,7 +31,9 @@ class CategoryController extends Controller {
         if ($request->hasFile('category_img')) {
            // $path = $request->file('category_img')->store('images/category_imgs', 'public');
            // $validated['category_img'] = asset('storage/' . $path);
-	   $validated['category_img'] = $this->saveCategoryImage($request->file('category_img'));
+	       // $validated['category_img'] = $this->saveCategoryImage($request->file('category_img'));
+            $path = $request->file('category_img')->store('category_imgs', 's3');
+            $fullUrl = Storage::disk('s3')->url($path);
         }
 
         // ログイン中のユーザーIDを使う
