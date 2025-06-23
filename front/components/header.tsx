f664857a899292ff4a2f3d80925150c322e98c9c
KaGH6 <kam85933@gmail.com>
2025-06-23 21:57:45 +0900

    revised

--- front/components/header.tsx
+++ front/components/header.tsx
@@ -25,7 +25,9 @@ export default function Header({ selectedCards }: HeaderProps) { // selectedCard
         if (pathname === "/") return "ホーム";
         if (pathname === "/menu") return "メニュー";
         if (pathname.startsWith("/categories") && pathname.includes("/cards")) return "カード 一覧";
+        if (pathname.startsWith("/cards") && pathname.includes("/edit")) return "カード 編集";
         if (pathname.startsWith("/categories")) return "カテゴリー 一覧";
+        if (pathname.startsWith("/categories") && pathname.includes("/edit")) return "カテゴリー 編集";
         if (pathname.startsWith("/checklists") && pathname.match(/^\/checklists\/\d+/)) return "チェックリスト詳細";
         if (pathname.startsWith("/checklists")) return "チェックリスト 一覧";
         return ""; // デフォルト
