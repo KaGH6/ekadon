9c7bd47aace4a6bae17e57d5153b9d688f07af0b
KaGH6 <kam85933@gmail.com>
2025-07-11 22:56:01 +0900

    updated

--- front/app/auth/login/page.tsx
+++ front/app/auth/login/page.tsx
@@ -22,10 +22,10 @@ export default function Login() {
     const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault(); // フォーム送信時のページリロードを防ぐ
 
-        // CSRF Cookieを最初に取得
-        await axios.get(`https://api.ekadon.com/sanctum/csrf-cookie`, {
-            withCredentials: true
-        });
+        // // CSRF Cookieを最初に取得(Laravel Sanctum用。JWT認証では不要。)
+        // await axios.get(`https://api.ekadon.com/sanctum/csrf-cookie`, {
+        //     withCredentials: true
+        // });
 
         // ログインリクエスト送信
         try {
@@ -40,12 +40,15 @@ export default function Login() {
             // トークンの取得と保存
             // Laravelからの返事（JSON）にあるtokenを取り出す
             const token = res.data.token;
+
+            // 1.トークンを保存
             localStorage.setItem("token", token);
 
+            // 2. axiosに自動でトークンを付けるようにする
             // axiosInstance の Authorization ヘッダーを更新
             axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-            router.push("/"); // ログイン後、ホーム画面に遷移
+            router.push("/"); // 3.ログイン後、ホーム画面に遷移
         } catch (err: any) {
             setError("ログインに失敗しました");
         }
